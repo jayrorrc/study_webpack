d35d42d35423de5bb6c0c4b6f7f300ef720325e2
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2019-05-30 17:30:40 -0300

    Create index as template

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,9 +2,21 @@ const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let plugins = [];
+
+plugins.push(new HtmlWebpackPlugin({
+    hash: true,
+    minify: {
+        html5: true,
+        collapseWhitespace: true,
+        removeComments: true,
+    },
+    filename: 'index.html',
+    template: __dirname + '/main.html'
+}));
 
 plugins.push(
     new extractTextPlugin("styles.css")
@@ -44,7 +56,6 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
     },
     module: {
         rules: [
